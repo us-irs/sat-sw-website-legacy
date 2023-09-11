198d67762d46456fc988b65b054f489aa56daa84
Nadine Eunuos <ne035@hdm-stuttgart.de>
2023-09-11 21:28:07 +0200

    update

--- index.js
+++ index.js
@@ -6,9 +6,21 @@ const fsfw_content = document.getElementById("fsfw-content");
 fsfw_click.addEventListener("click", () => {
   fsfw_content.style.display = "flex";
   sat_content.style.display = "none";
+
+  fsfw_click.style.backgroundColor = "#555"
+  fsfw_click.style.color = "white"
+
+  sat_click.style.backgroundColor = "#f1f1f1"
+  sat_click.style.color = "#000"
+
 });
 
 sat_click.addEventListener("click", () => {
   fsfw_content.style.display = "none";
   sat_content.style.display = "flex";
+
+  fsfw_click.style.backgroundColor = "#f1f1f1"
+  fsfw_click.style.color = "#000"
+  sat_click.style.backgroundColor = "#555"
+  sat_click.style.color = "white"
 });
